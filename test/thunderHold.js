4b7d5d585fb8916dc68ec31fe139b0b5a59deade
matias.nunez <matias.nunez@intraway.com>
2021-07-17 16:33:01 -0300

    Add balance test and refactor

--- test/thunderHold.js
+++ test/thunderHold.js
@@ -1,5 +1,7 @@
+// const Web3 = require('web3');
 const ThunderHold = artifacts.require("./ThunderHold.sol");
 let thunderHold;
+// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 before( async () => {
     thunderHold = await ThunderHold.new(
@@ -27,11 +29,17 @@ contract('ThunderHold Tests', function(accounts){
         var new_user = accounts[1];
         await thunderHold.invest(
             "0x94B50Ad34FD502831471B6f5583316820C77B94E", 0,
-            {from: new_user, value: String(11e16)}
+            {from: new_user, value: String(10e16), gas: 500000} //Deposito 1 ETH
         );
 
         var new_user_deposits = await thunderHold.getUserAmountOfDeposits(new_user);
         assert.equal(new_user_deposits, 1 , "deposit failed");
+    });
+
+    it("has founding balance", async () => {
+        var actualBalance = await web3.eth.getBalance(thunderHold.address);
+        var expectedBalance = await web3.utils.toWei('0.09', 'ether'); // Me queda 0.9 ETH ya descontamos el 10%
+        assert.deepEqual(actualBalance, expectedBalance, "Balance incorrect!");
     });
 
     });
