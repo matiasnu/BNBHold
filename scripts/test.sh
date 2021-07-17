f5bb12ee7b829a8e5cf80e804c7fbd691812c7c8
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-17 20:18:21 -0300

    add test cases ti ci workflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 
 
-echo "###### Running truffle-docker [v."
+echo "###### Running truffle migrate"
 
+echo "[INFO] Deploy smart contracts (truffle migrate --reset --compile-all --network $NETWORK) ..."
+output=$(truffle migrate --reset --compile-all --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
+echo "output: $output"
 
 echo "[INFO] Exec truffle --network $NETWORK) ..."
 output=$(truffle test --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
-
 
 echo "output: $output"
