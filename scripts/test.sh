f2e56ba9fe942aa04e6b6a2d6dee2687bff6fa4f
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-17 21:40:50 -0300

    trying to detect test failing in pipeline

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,10 @@ echo "###### Running truffle migrate"
 echo "[INFO] Deploy smart contracts (truffle migrate --reset --compile-all --network $NETWORK) ..."
 output=$(truffle migrate --reset --compile-all --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
+echo "output: $output"
+
+sleep 1
+
 echo "[INFO] Exec truffle --network $NETWORK) ..."
 output=$(truffle test --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
