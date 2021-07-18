a5ec19cf6ce29f651d6cb42087b09821b2ec47d7
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-17 23:36:08 -0300

    tt

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,11 +6,12 @@ echo "###### Running truffle migrate"
 echo "[INFO] Deploy smart contracts (truffle migrate --reset --compile-all --network $NETWORK) ..."
 output=$(truffle migrate --reset --compile-all --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
-echo "output: $output"
 
-sleep 1
+sleep 10
 
 echo "[INFO] Exec truffle --network $NETWORK) ..."
 output=$(truffle test --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
 echo "output: $output"
+
+echo "::set-output name=TESTRESULT::$output"
