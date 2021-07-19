2c656ecebbebc7d24e296ef9536619ee2e63b4a6
matias.nunez <matias.nunez@intraway.com>
2021-07-19 17:23:43 -0300

    Updates

---
+++ precache-manifest.2be62257ed529f7b117227a1de6d14f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62a835a8625ab9e7c8ee663ceaac1a54",
+    "url": "/BNBHold/index.html"
+  },
+  {
+    "revision": "3b0aaa9e00b7f1921afa",
+    "url": "/BNBHold/static/css/main.ca2a323d.chunk.css"
+  },
+  {
+    "revision": "ce59a3c9c483fdd7baae",
+    "url": "/BNBHold/static/js/2.74137530.chunk.js"
+  },
+  {
+    "revision": "3b0aaa9e00b7f1921afa",
+    "url": "/BNBHold/static/js/main.ddb8f712.chunk.js"
+  },
+  {
+    "revision": "3b04ee55b4c6b4a638c7",
+    "url": "/BNBHold/static/js/runtime-main.c82a0ade.js"
+  },
+  {
+    "revision": "bddf4e2418784ecfb03da7f45e5ad715",
+    "url": "/BNBHold/static/media/chatIcon.bddf4e24.png"
+  },
+  {
+    "revision": "b2d83ecd1c59454765a96a1ba78d1689",
+    "url": "/BNBHold/static/media/pinguino-logo.b2d83ecd.png"
+  }
+]);
