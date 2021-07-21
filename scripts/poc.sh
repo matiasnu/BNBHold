f78993c9ca54d13cbf03e2eb26b8081e041badfc
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-20 22:14:58 -0300

    eeee

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -2,7 +2,6 @@
 
 docker-compose -f docker-compose-gh-pages.yml build \
 --build-arg GIT_SSH_KEY="$(cat ~/.ssh/id_rsa)" \
---build-arg GIT_PUBLIC_SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" \
 --build-arg GITHUB_CONFIG_USERNAME="Mariano Di Maggio" \
 --build-arg GITHUB_CONFIG_EMAIL="mariano.dim@gmail.com" \
 gh-pages-deploy
