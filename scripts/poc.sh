08129899b8623b1b3e9c12ace236dc9ddf975fd4
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-20 21:07:50 -0300

    lpm

--- scripts/poc.sh
+++ scripts/poc.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-export output="error 1"
-
-echo "output: $output"
-
-echo "::set-output name=TESTRESULT::$output"
+docker-compose -f docker-compose-gh-pages.yml build \
+--build-arg GIT_SSH_KEY="$(cat ~/.ssh/id_rsa)" \
+--build-arg GIT_PUBLIC_SSH_KEY="$(cat ~/.ssh/id_rsa.pub)" \
+--build-arg GITHUB_CONFIG_USERNAME="Mariano Di Maggio" \
+--build-arg GITHUB_CONFIG_EMAIL="mariano.dim@gmail.com" \
+gh-pages-deploy
