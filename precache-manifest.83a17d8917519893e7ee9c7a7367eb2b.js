c590e9a37cef2d02aca6731e0de66c7e195e5a7d
Matiasnu <Matiasnu@users.noreply.github.com>
2021-07-26 00:09:42 -0300

    deploy: db031afbbcc3ab8a9572265f23169f04514f0edd

---
+++ precache-manifest.83a17d8917519893e7ee9c7a7367eb2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbeff9e44cd8befdad3eb52d89376181",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345542f44bb57ecec9a6",
+    "url": "/static/css/main.5528e245.chunk.css"
+  },
+  {
+    "revision": "bbfa0f18ce40ac942af3",
+    "url": "/static/js/2.848aaf10.chunk.js"
+  },
+  {
+    "revision": "345542f44bb57ecec9a6",
+    "url": "/static/js/main.7de18653.chunk.js"
+  },
+  {
+    "revision": "21fd375bcf92cdf5b3a4",
+    "url": "/static/js/runtime-main.471d5b06.js"
+  },
+  {
+    "revision": "bddf4e2418784ecfb03da7f45e5ad715",
+    "url": "/static/media/chatIcon.bddf4e24.png"
+  },
+  {
+    "revision": "b2d83ecd1c59454765a96a1ba78d1689",
+    "url": "/static/media/pinguino-logo.b2d83ecd.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BNBHold/precache-manifest.17ca83e990c6bf902188a56feb14014e.js"
+  "/precache-manifest.83a17d8917519893e7ee9c7a7367eb2b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/BNBHold/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
