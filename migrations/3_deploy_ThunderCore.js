64b8d6342d99814434f6d3ef5f5f4121da2f8656
Mariano Andres Di Maggio <mariano.dim@gmail.com>
2021-07-27 21:16:58 -0300

    generate new package.json for testing to fix migration

--- migrations/3_deploy_ThunderCore.js
+++ migrations/3_deploy_ThunderCore.js
@@ -6,7 +6,7 @@ module.exports = function (deployer, network) {
 
     deployer.deploy(
       ThunderHold,
-      "0x22d491Bde2303f2f43325b2108D26f1eAbA1e32b",
+      "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1",
       Math.round(new Date().getTime() / 1000)
     );
   } else {
--- scripts/test.sh
+++ scripts/test.sh
@@ -3,11 +3,13 @@
 
 echo "###### Running truffle migrate"
 
-echo "[INFO] Deploy smart contracts (truffle migrate --reset --compile-all --network $NETWORK) ..."
-output=$(truffle migrate --reset --compile-all --network $NETWORK) || { echo '[ERROR] Failed to migrate' ; exit 1; }
+tail -F waitfile
+
+echo "[INFO] Deploy smart contracts (truffle migrate --reset --compile-all --network $NETWORK -f 3) ..."
+output=$(truffle migrate --reset --compile-all --network $NETWORK -f 3) || { echo '[ERROR] Failed to migrate' ; exit 1; }
 
 
-sleep 10
+# sleep 10
 
 echo "[INFO] Exec truffle test--network $NETWORK) ..."
 output=$(truffle test --network $NETWORK) || { echo '[ERROR] Failed testing contract' ; exit 1; }
